879eb479808494462cd472d8c4d0952d069dfb9c
Wildrimak <finalwildrimak@gmail.com>
2021-09-11 16:23:01 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/wildrimak/jwt/SimplificationJwtTokenApplication.java
@@ -0,0 +1,13 @@
+package br.com.wildrimak.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplificationJwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplificationJwtTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wildrimak/jwt/SimplificationJwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wildrimak.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplificationJwtTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
